14a724cdeeb6cf8b99e30b1753adc5e0c37e1e1d
DVChannel <channelgonzalezjimenez100@gmail.com>
2022-08-03 21:17:31 -0500

    Corrigiendo models

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Question(models.Model):
-  question_text = models.Charfield(max_length=200)
-  pub_date = models.DataTimeField("date published")
-  
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField("date published")
+
 class Choices(models.Model):
-  question = models.ForeignKey(Question, on_delete=models.CASCADE)
-  choice_text = models.CharField(max_length=200)
-  votes = models.Integerfield(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0) 
