431f2d814f56ad7e34ee02839555b2959d8a1921
DVChannel <channelgonzalezjimenez100@gmail.com>
2022-08-03 21:56:29 -0500

    error 404

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -10,3 +10,12 @@ def index(request):
 
 
 def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html",{
+        "question": question
+    })
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero{question_id}")
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando la pregunta numero {question_id}")
